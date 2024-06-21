c23e15aec3fe1dc1dc9cae4d73916ac9aa0d3d8e
ssssvio <r.savio@proton.me>
2024-06-21 09:55:07 -0300

    feat: Add CarsController for managing car resources
    The code changes include:
    - Adding a new file  in the  directory
    - Importing and registering the  in the
    - Implementing CRUD operations for managing car resources in the
    
    This commit adds the  to the application, allowing users to perform operations such as listing all cars, retrieving a specific car, creating a new car, updating an existing car, and deleting a car.
    
    Note: The recent user commits and repository commits do not provide any additional context for the commit message.

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
+
+@Controller('cars')
+export class CarsController {
+  @Get()
+  findAll(@Res() response) {
+    return response.status(200).json({ message: 'Listagem de carros' });
+  }
+
+  @Get(':id/:car')
+  findOne(@Param('id') id: string, @Param('car') car: string) {
+    return `Carro é o ${car} ano ${id}`;
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return body;
+  }
+
+  @Put(':id')
+  updade(@Param('id') id: string, @Res() response: any, @Body() body: any) {
+    return response
+      .status(200)
+      .json({ message: `Carro com id ${id} atualizado`, body });
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Carro com id ${id} deletado`;
+  }
+}
