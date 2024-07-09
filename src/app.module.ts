6e8c5571f769a049cc91a1c5246fdc83eb269e30
ssssvio <r.savio@proton.me>
2024-07-09 14:51:03 -0300

    Refactor CarsController to use number type for id parameter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CarsModule } from './cars/cars.module';
-import { DatabaseModule } from './database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './config/typeorm.factory';
 
 @Module({
-  imports: [CarsModule, DatabaseModule],
+  imports: [CarsModule, TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
--- src/config/typeorm.factory.ts
+++ src/config/typeorm.factory.ts
@@ -1,23 +1,23 @@
-// import 'dotenv/config';
-// import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import 'dotenv/config';
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-// export class TypeOrmConfigService implements TypeOrmOptionsFactory {
-//   createTypeOrmOptions(): TypeOrmModuleOptions {
-//     return {
-//       type: 'mssql',
-//       host: process.env.TYPEORM_HOST,
-//       port: +process.env.TYPEORM_PORT,
-//       username: process.env.TYPEORM_USERNAME,
-//       password: process.env.TYPEORM_PASSWORD,
-//       database: process.env.TYPEORM_DATABASE,
-//       synchronize: true,
-//       logging: true,
-//       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-//       migrations: [__dirname + '/migrations/*{.ts,.js}'],
-//       subscribers: [],
-//       extra: {
-//         trustServerCertificate: true,
-//       },
-//     };
-//   }
-// }
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mssql',
+      host: process.env.TYPEORM_HOST,
+      port: +process.env.TYPEORM_PORT,
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      synchronize: true,
+      logging: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      migrations: [__dirname + '/migrations/*{.ts,.js}'],
+      subscribers: [],
+      extra: {
+        trustServerCertificate: true,
+      },
+    };
+  }
+}
