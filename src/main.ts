a8595417add134c5a6c1f4d886a8242aaf6bd65c
ssssvio <r.savio@proton.me>
2024-07-22 15:39:26 -0300

    chore: Fix typo in import statement for AllExceptionsFilter

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import { AllExceptionsFilter } from './common/filter/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
