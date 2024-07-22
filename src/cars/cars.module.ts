3bd9ad6017dd673c9d283a068abe1d9efd547d80
ssssvio <r.savio@proton.me>
2024-07-22 15:19:14 -0300

    feat: Update CarsModule providers
    The code changes include:
    - Adding CreateCarsService, DeleteCarsService, UpdateCarsService, and FindCarsService to the providers array in the CarsModule
    
    This commit updates the CarsModule to include the newly added services for creating, deleting, updating, and finding cars.

--- src/cars/cars.module.ts
+++ src/cars/cars.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
-import { CarsService } from './cars.service';
 import { CarsController } from './cars.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Cars } from './entities/cars.entity';
+import { CreateCarsService } from './services/create-car.service';
+import { DeleteCarsService } from './services/delete-car.service';
+import { UpdateCarsService } from './services/update-car.service';
+import { FindCarsService } from './services/find-car.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Cars])],
   controllers: [CarsController],
-  providers: [CarsService],
+  providers: [
+    CreateCarsService,
+    DeleteCarsService,
+    UpdateCarsService,
+    FindCarsService
+  ],
 })
 export class CarsModule { }
