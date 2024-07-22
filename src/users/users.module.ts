388d25cc2a2a146c349420bc02b15e3be917cc99
ssssvio <r.savio@proton.me>
2024-07-22 15:23:13 -0300

    chore: Update UsersModule providers
    The code changes include:
    - Adding CreateUsersService, DeleteUsersService, UpdateUsersService, and FindUsersService to the providers array in the UsersModule
    
    This commit updates the UsersModule to include the newly added services for creating, deleting, updating, and finding users.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersController } from './users.controller';
+import { Users } from './entities/users.entity';
+import { CreateUsersService } from './services/create-user.service';
+import { DeleteUsersService } from './services/delete-users.service';
+import { UpdateUsersService } from './services/update-user.service';
+import { FindUsersService } from './services/find-users.service';
 
-@Module({})
-export class UsersModule {}
+@Module({
+  imports: [TypeOrmModule.forFeature([Users])],
+  controllers: [UsersController],
+  providers: [
+    CreateUsersService,
+    DeleteUsersService,
+    UpdateUsersService,
+    FindUsersService
+  ],
+})
+export class UsersModule { }
+
+
