4a08d8d86c05250ca464d83d4fc9c408685bf4ac
ssssvio <r.savio@proton.me>
2024-07-31 16:51:59 -0300

    refactor: Update TrimPipe to handle trimming of object values
    The code changes include:
    - Modifying the TrimPipe to handle trimming of object values in addition to string values
    
    This commit refactors the TrimPipe to ensure that both string values and object values are properly trimmed, improving the functionality and consistency of the pipe.

--- src/common/pipes/trim-pipes.ts
+++ src/common/pipes/trim-pipes.ts
@@ -4,14 +4,15 @@ export class TrimPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
     if (typeof value === 'string') {
       return value.trim();
-    }
+    };
+
     if (typeof value === 'object' && value !== null) {
       for (const key in value) {
         if (typeof value[key] === 'string') {
           value[key] = value[key].trim();
         }
       }
-    }
-    return value;
+    };
+    return value
   };
-}
+};
